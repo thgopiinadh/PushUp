cc4e4d631f8b147910adbf94be3f93ea75e2c3d2
GN Reddy <thgopiinadh@gmail.com>
2018-04-16 12:53:11 +0530

    Initial commit

---
+++ app/src/main/java/com/learnexo/pushup/MainActivity.java
@@ -0,0 +1,16 @@
+package com.learnexo.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void testUpload(){
+        int i=1+1;
+    }
+}
