a4212441d69494a01d07755e39fec6222aa7f9c1
GN Reddy <thgopiinadh@gmail.com>
2018-04-16 13:07:12 +0530

    Modified MainActivity Added a new File TestAddFile.java

--- app/src/main/java/com/learnexo/pushup/MainActivity.java
+++ app/src/main/java/com/learnexo/pushup/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     }
     private void testUpload(){
         int i=1+1;
+        int ii=2+2;
+        int iii=i+ii;
     }
 }
---
+++ app/src/main/java/com/learnexo/pushup/TestAddFile.java
@@ -0,0 +1,8 @@
+package com.learnexo.pushup;
+
+/**
+ * Created by GNREDDY on 16/04/18.
+ */
+
+public class TestAddFile {
+}
